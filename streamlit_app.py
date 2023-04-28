98b6413c207b35cb409cf1d0694ecea19860e4bf
ilhem kacimi <78962246+ilhem98@users.noreply.github.com>
2023-04-28 20:31:15 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,10 @@ with header_mid:
 
 @st.cache_resource
 def get_data():
+    g = Github("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")
+    repo = g.get_repo("ilhem98/PatientDataAnalytics")
+    file = repo.get_contents("blob/main/30-Days-DExcomClarity_CGM.csv")
+
     df = pd.read_csv('https://github.com/ilhem98/PatientDataAnalytics/blob/main/30-Days-DExcomClarity_CGM.csv', sep =';')
     glucose = df[['DataDtTm', 'CGM']]
     return glucose
